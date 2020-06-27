2b4cc3a3a3901286706a4481ebedbc0d6447ae37
Rito <ruangrit@opendream.co.th>
2020-06-27 15:16:48 +0700

    path alias

--- sites/all/modules/rit_module/rithook/js/products_node_form.js
+++ sites/all/modules/rit_module/rithook/js/products_node_form.js
@@ -1,6 +1,19 @@
 $ = jQuery;
 $(document).ready(function() {
 
+
+	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+	
 	var code;
 	var numberCode = 'XXXX';
 	var shopCode = 'X';
