568938d6ddb76d5f624b56c5a1dad96f1ac1caee
Rito <ruangrit@opendream.co.th>
2020-07-08 18:00:23 +0700

    paragraph module

--- sites/all/modules/rit_module/rithook/js/suppliers_node_form.js
+++ sites/all/modules/rit_module/rithook/js/suppliers_node_form.js
@@ -1,5 +1,13 @@
 $ = jQuery;
 $(document).ready(function() {
+
+	if ($('#field-p-coordinators-values').length == 0) {
+
+		$('#edit-field-p-coordinators-und-add-more > button').trigger('mousedown');
+
+	}
+
+
 	$(".various").fancybox({
 		maxWidth	: 800,
 		maxHeight	: 600,
