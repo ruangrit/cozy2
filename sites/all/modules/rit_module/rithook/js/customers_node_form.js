aab608fdbcc835d63e430454e8088bae484a83d6
Rito <ruangrit@opendream.co.th>
2020-07-09 23:33:02 +0700

    customer email validate

--- sites/all/modules/rit_module/rithook/js/customers_node_form.js
+++ sites/all/modules/rit_module/rithook/js/customers_node_form.js
@@ -3,4 +3,27 @@ $(document).ready(function() {
 	$('.group-name').children().addClass('col-xs-4');
 
 	$('.group-customer-birth').children().addClass('col-xs-4');
+
+	/*$('#field-email-values').find('input').each(function( index ) {
+  		//console.log( index + ": " + $( this ).text() );
+  		$(this).on( "blur", function() {
+  			console.log( $( this ).val() );
+		});
+	});
+	$('input').on( "blur", function() {
+  			console.log( $( this ).val() );
+	});
+	*/
+
+$( document ).on( "blur", "#edit-field-email  input", function() {
+	if( !validateEmail($( this ).val()) ) {
+
+		$(this).after('<div class="alert alert-block alert-dismissible alert-danger messages error"><a class="close" data-dismiss="alert" href="#">×</a><h4 class="element-invisible">Error message</h4>'+ $(this).val() +' is not a valid email address</div>');
+	}
+});
+
+	function validateEmail($email) {
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+	  	return emailReg.test( $email );
+	}
 });
