e8f27a76a11f6885109296d438c6550463d4c272
Rito <ruangrit@opendream.co.th>
2020-07-16 15:37:31 +0700

    customer two form

--- sites/all/modules/rit_module/rithook/js/customers_node_form.js
+++ sites/all/modules/rit_module/rithook/js/customers_node_form.js
@@ -5,16 +5,43 @@ $(document).ready(function() {
 	$('.group-customer-birth').children().addClass('col-xs-4');
 
 
-$( document ).on( "blur", "#edit-field-email  input", function() {
-	$('.error-email').remove();
-	if( !validateEmail($( this ).val()) ) {
+	$( document ).on( "blur", "#edit-field-email  input", function() {
+		$('.error-email').remove();
+		if( !validateEmail($( this ).val()) ) {
 
-		$('#edit-field-email').before('<div class="alert alert-block alert-dismissible alert-danger messages error error-email"><a class="close" data-dismiss="alert" href="#">×</a><h4 class="element-invisible">Error message</h4>'+ $(this).val() +' is not a valid email address</div>');
-	}
-});
+			$('#edit-field-email').before('<div class="alert alert-block alert-dismissible alert-danger messages error error-email"><a class="close" data-dismiss="alert" href="#">×</a><h4 class="element-invisible">Error message</h4>'+ $(this).val() +' is not a valid email address</div>');
+		}
+	});
 
 	function validateEmail($email) {
 		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 	  	return emailReg.test( $email );
 	}
+
+
+	var getUrlParameter = function getUrlParameter(sParam) {
+	    var sPageURL = window.location.search.substring(1),
+	        sURLVariables = sPageURL.split('&'),
+	        sParameterName,
+	        i;
+
+	    for (i = 0; i < sURLVariables.length; i++) {
+	        sParameterName = sURLVariables[i].split('=');
+
+	        if (sParameterName[0] === sParam) {
+	            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+	        }
+	    }
+	};
+
+	var csf = getUrlParameter('csf');
+	if (csf == 'web' || csf == 'shop') {
+		$('.multiple-fields-remove-button').hide();
+		$('.field-add-more-submit').hide();
+	}
+
+
+
+
+
 });
