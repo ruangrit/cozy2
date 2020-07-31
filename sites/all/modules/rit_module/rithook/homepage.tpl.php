61030c1033338c2902072981de70261f0711da6c
Rito <ruangrit@opendream.co.th>
2020-07-31 16:21:28 +0700

    change text title

--- sites/all/modules/rit_module/rithook/homepage.tpl.php
+++ sites/all/modules/rit_module/rithook/homepage.tpl.php
@@ -8,7 +8,7 @@ $nodes = node_load_multiple(array(), array('type' => 'products'));
 foreach ($nodes as $node) {
 	
 	$path = drupal_get_path_alias('node/'.$node->nid);
-	print '<div class="container">';
+	print '<div class="container" style="border-bottom: 1px solid #eee; padding-top: 20px;">';
 		print '<div class="row marb20">';
 			print '<div class="col-xs-4"><a href="'.$path.'">';
 				print theme('image_style', 
@@ -21,11 +21,11 @@ foreach ($nodes as $node) {
 			print '</a></div>';
 
 			print '<div class="col-xs-8">';
-				print '<b>PRODUCT CODE:</b> ' . $node->field_product_code['und'][0]['value'];
-				print '</br>';
-				print '<b>PRODUCT NAME:</b> ' . $node->title;
-				print '</br>';
-				print '<b>PRICE:</b> ' .$node->field_product_price['und'][0]['value'].' THB';
+
+				print '<div style="color: #98B8CA; font-size: 16px; line-height: 1.5em;">' . $node->field_product_code['und'][0]['value'] . '</div>';
+				print '<div style="color: #666666; font-size: 18px; font-weight: bold; line-height: 1.5em;">PRODUCT NAME:</b> ' . $node->title . '</div>';
+				print '<div style="color: #989898; font-size: 16px; line-height: 1.5em;"> THB ' .number_format($node->field_product_price['und'][0]['value']).' </div>';
+
 			print '</div>';
 		print '</div>';
 	print '</div>';
