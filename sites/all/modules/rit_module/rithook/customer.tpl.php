f4870019180a63fbcf9b803b1b0620b53e845436
Rito <ruangrit@opendream.co.th>
2020-08-05 18:00:24 +0700

    customer search

--- sites/all/modules/rit_module/rithook/customer.tpl.php
+++ sites/all/modules/rit_module/rithook/customer.tpl.php
@@ -1,10 +1,95 @@
 <?php
+
+// search value
+
+$search_name = '';
+$search_phone = '';
+if (isset($_GET['title'])) {
+	$search_name  = $_GET['title'];
+}
+if (isset($_GET['field_in_contact_number'])) {
+	$search_phone = $_GET['field_in_contact_number'];
+}
+
+
+?>
+
+<div class="search-form container">
+	<div class="row">
+		<div class="col-xs-12 text-right">
+		<form method="GET">
+			<b>Search customer by:</b>
+		
+			
+				Name: <input type="text" name="title" value="<?php print $search_name;?>">
+				Contact number: <input type="text" name="field_in_contact_number" value="<?php print $search_phone; ?>">
+				<input type="submit" value="Search">
+			</form>
+		</div>
+
+	</div>
+</div>
+
+
+<?php
 global $user;
 if ($user->uid == 0) {
 	//print 'Login to view product.';
 	//return false;
 }
-$nodes = node_load_multiple(array(), array('type' => 'customers'));
+
+/*
+$query = new EntityFieldQuery();
+
+$query->entityCondition('entity_type', 'node')
+->entityCondition('bundle', 'customers')
+->propertyCondition('status', 1)
+->fieldCondition('field_in_contact_number', 'value', $search_phone. '%', 'like')
+->propertyCondition('title', $search_name. '%', 'like')
+->range(0, 10)
+->addMetaData('account', user_load(1)); // Run the query as user 1.
+
+$result = $query->execute();
+*/
+
+
+$sql = '
+	SELECT DISTINCT n.nid 
+	FROM {node} n   
+	LEFT JOIN {field_data_field_in_contact_number} ct
+	ON n.nid = ct.entity_id
+	WHERE n.type = :type 
+';
+
+$params = array(':type'  => 'customers');
+
+if ($search_name != '' AND $search_phone != '') {
+	$sql .= 'AND (n.title LIKE :title AND ct.field_in_contact_number_value LIKE :phone)';
+	$params[':title'] = '%'.$search_name.'%';
+	$params[':phone'] = '%'.$search_phone.'%';
+
+}
+else if($search_name != '') {
+	$sql .= 'AND (n.title LIKE :title)';
+	$params[':title'] = '%'.$search_name.'%';
+
+}
+else if($search_phone != ''){
+	$sql .= 'AND (ct.field_in_contact_number_value LIKE :phone)';
+	$params[':phone'] = '%'.$search_phone.'%';
+}
+
+
+$result = db_query($sql, $params);
+
+$nids = array();
+foreach ($result as $record) {
+	$nids[] = $record->nid;
+}
+
+
+
+$nodes = node_load_multiple($nids);
 
 $total_node = count($nodes);
 $run_num = 1;
