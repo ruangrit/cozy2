3dcd8b7ee18cb950576952710a8c45d7fa3e008c
Rito <ruangrit@opendream.co.th>
2020-08-08 16:49:01 +0700

    coordinator form

--- sites/all/modules/rit_module/rithook/js/suppliers_node_form.js
+++ sites/all/modules/rit_module/rithook/js/suppliers_node_form.js
@@ -1,12 +1,25 @@
 $ = jQuery;
 $(document).ready(function() {
 
+	$('.group-col2').children().addClass('col-xs-6');
+
 	if ($('#field-p-coordinator-values').length == 0) {
-		//console.log('click');
-		$('#edit-field-p-coordinator-und-add-more > button').trigger('mousedown');
+
+		console.log('click');
+		$('#edit-field-p-coordinator-und-add-more').find('button').trigger('mousedown');
 
 	}
 
+	$('#edit-field-p-coordinator').find('button').click(function () {
+
+		console.log('sssss');
+	});
+
+	$('body').on('DOMNodeInserted', '.field-name-field-p-coordinator', function () {
+      	
+      	$('.group-col2').children().addClass('col-xs-6');
+		//console.log('aaaa');
+	});
 
 	$(".various").fancybox({
 		maxWidth	: 800,
